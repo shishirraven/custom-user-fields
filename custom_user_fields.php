c5723c621653756a5736dfc45f5e7bae8a09f0cd
Shishir Raven <shishir.raven@gmail.com>
2022-11-06 02:46:30 +0530

    Update custom_user_fields.php

--- custom_user_fields.php
+++ custom_user_fields.php
@@ -3,7 +3,22 @@
 /**
  * plugin name: Custom User Fields
  * plugin URI: iamshishir.com
- * version : 1
+ 
  * description: Add Visual Fields to User Profile, User Edit and, New User Creation. 
+ * @author            Shishir Raven
+ * @license           GPL-3.0-or-later
+ * @wordpress-plugin
+ * Plugin Name:       Plugin Name
+ * Plugin URI:        https://iamshishir.com/visual-user-fields-wordpress-plugin/
+ * Description:       Description of the plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Shishir Raven
+ * Author URI:        https://iamshishir.com
+ * Text Domain:       custom-user-fields
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.txt
+ * Update URI:        https://iamshishir.com/visual-user-fields-wordpress-plugin/
  */
 include("includes/dynamicFields.php");
