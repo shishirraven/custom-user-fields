2d9affe3353a15a1d31e9afffd3b56172261856e
Shishir Raven <shishir.raven@gmail.com>
2022-11-06 02:36:20 +0530

    Update custom_user_fields.php

--- custom_user_fields.php
+++ custom_user_fields.php
@@ -3,6 +3,7 @@
 /**
  * plugin name: Custom User Fields
  * plugin URI: iamshishir.com
+ * version : 1
  * description: Add Visual Fields to User Profile, User Edit and, New User Creation. 
  */
 include("includes/dynamicFields.php");
